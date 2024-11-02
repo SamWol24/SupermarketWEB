62ad6c1297f6c01daa1f96f9a1210ff121b78a23
Samir Agudelo Curaca <samiragudelo21@gmail.com>
2024-11-02 01:12:32 -0500

    primer commit

---
+++ Data/SumpermarketContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Data
+{
+    public class SumpermarketContext
+    {
+        public class SupermarketContext : DbContext
+        {
+            public SupermarketContext(DbContextOptions options) : base(options)
+            {
+            }
+            public DbSet<Category> Categories { get; set; }
+            public DbSet<Product> Products { get; set; }
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketWEB.Models
+{
+    public class Product
+    {
+        // [key] -> Anotacion si la propiedad no se llama Id, ejemplo ProductId
+
+        public int Id { get; set; } // Sera la llave primaria
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(6,2)")]
+
+        public decimal Price { get; set; }
+        public int Stork { get; set; }
+        public int CategoryId { get; set; } // Sera la llave foranea
+        public Category Category { get; set; } // Propiedad de navegacion
+    }
+}
