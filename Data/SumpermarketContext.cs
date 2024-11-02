1ec4d268fc13f278370301f73308ab6aec21717e
Samir Agudelo Curaca <samiragudelo21@gmail.com>
2024-11-02 11:43:46 -0500

    Index Categories

--- Data/SumpermarketContext.cs
+++ Data/SumpermarketContext.cs
@@ -11,7 +11,7 @@ namespace SupermarketWEB.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<PayMode> PayModes { get; set; }
-        public DbSet<Provider> Providers { get; set; }
+      //  public DbSet<PayMode> PayModes { get; set; }
+      //  public DbSet<Provider> Providers { get; set; }
     }
 }
