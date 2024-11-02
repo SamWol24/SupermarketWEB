7466940a06f5c2dcf7290071a5844f8cabc092e1
Samir Agudelo <samiragudelo21@gmail.com>
2024-11-02 11:48:42 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using SupermarketEF.Data;
-
-namespace SupermarketEF
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            var serviceProvider = new ServiceCollection()
-                .AddDbContext<SupermarketContext>(options =>
-                    options.UseSqlServer("Server=localhost;Database=SupermarketEF;Trusted_Connection=True;"))
-                .BuildServiceProvider();
-
-            using (var context = serviceProvider.GetService<SupermarketContext>())
-            {
-                // Aquí puedes realizar operaciones con el contexto si lo deseas
-                Console.WriteLine("DbContext creado exitosamente.");
-            }
-=======
 using Microsoft.EntityFrameworkCore;
 using SupermarketWEB.Data;
 
@@ -62,7 +39,6 @@ namespace SupermarketWEB
             app.MapRazorPages();
 
             app.Run();
->>>>>>> 6182bfadce7588acb39cfb53303d29be2806ba20
         }
     }
 }
